d07aee27f8245e69232db15d8f04878836586c66
mbodji <ibumbodji@gmail.com>
2023-03-27 11:50:16 +0200

    adding code for database and generation sql

--- app.py
+++ app.py
@@ -10,3 +10,37 @@ app.secret_key = secrets.token_hex(16)
 
 app.config['DATABASE'] = 'database.db'
 
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+app.teardown_appcontext
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    db.commit()
+
+@click.command("init_db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+app.cli.add_command(init_db_command)
+
+# ----------------------------------End-----------------------------------------------
