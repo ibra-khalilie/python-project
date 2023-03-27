2c8a63b651cd58a75bef353c711e205e23d653b8
Yaya Sakho <yahyasakho97a@gmail.com>
2023-03-27 11:51:15 +0200

    app.py : initialization of the database

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
-import secrets
 import sqlite3
 
 import click
-from flask import (Flask, current_app, g, redirect, render_template, request,
-                   session, url_for)
+import secrets
+
+
+from flask import Flask, current_app, g
+
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-app.config['DATABASE'] = 'database.db'
+app.config["DATABASE"] = "database.db"
 
+# ---------------------------------Database---------------------------------------
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+app.teardown_appcontext
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+    db.commit()
+
+
+@click.command("init_db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
